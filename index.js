21ee8b78a9b8bc5531d363e5a6b63b12244dc972
Olajide A. Hammed <Olajide.A.Hammed@gmail.com>
2020-04-22 20:59:34 +0100

    Added a handwashin picture

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-// Created by Ayodele Salimon
-
 $.ajax({
-  url: "https://api.thevirustracker.com/free-api?countryTotal=IN",
+  url: "https://api.thevirustracker.com/free-api?countryTotal=NG",
   dataType: "json",
   success: function (data) {
     $(document).ready(function () {
@@ -12,15 +10,33 @@ $.ajax({
   },
 });
 
+/*
+var settings = {
+  async: true,
+  crossDomain: true,
+  url: "https://nigeria-covid-19.p.rapidapi.com/api/states",
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "nigeria-covid-19.p.rapidapi.com",
+    "x-rapidapi-key": "5ce2f575d5msh75c383fe93bd857p1ba446jsn12438cb3bced",
+  },
+};
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+*/
+
+var selectElem = document.getElementById("select");
 $.ajax({
   url: "https://covid19project.org.ng/api/endpoints/states",
   dataType: "json",
   success: function (res) {
-    $(document).ready(function () {
-      $("#s").text(res.Covid19NG[0].name);
-      $("#s1").text(res.Covid19NG[0].cases);
-      $("#s2").text(res.Covid19NG[0].death);
-      $("#s3").text(res.Covid19NG[0].recovered);
+    selectElem.addEventListener("change", function () {
+      var i = selectElem.selectedIndex - 1;
+      $("#s").text(res.Covid19NG[i].name);
+      $("#s1").text(res.Covid19NG[i].cases);
+      $("#s2").text(res.Covid19NG[i].death);
+      $("#s3").text(res.Covid19NG[i].recovered);
     });
   },
 });
